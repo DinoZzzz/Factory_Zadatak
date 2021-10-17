2deda99b509c2a2646e0a94b9169a9e1da2bcecf
Karlo Stefcic <karlo.stefcic@gmail.com>
2021-10-17 18:04:55 +0200

    working slider

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-$(function () {
-    var slick = $('.img-top').slick({
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        variableWidth: true,
-        appendArrows: $('#arrows')
-    });
+$(document).ready(function () {
+    const topImages = $('.img-top')
+    const bottomImages = $('.img-bot')
+    const leftBtn = $('.prev-btn')
+    const rightBtn = $('.next-btn')
 
-    $('.slick-prev').on('click', function () {
-        slick.slickNext();
-    });
-    $('.slick-next').on('click', function () {
-        slick.slickPrev();
-    });
+    $(rightBtn).click(() => {
+        $(rightBtn).prop('disabled', true)
+        let lastImageTop = $(".img-top .image").last()
+        $(lastImageTop).clone().prependTo(topImages)
+        $(lastImageTop).remove()
+        let lastImageBottom = $(".img-bot .image").last()
+        $(lastImageBottom).clone().prependTo(bottomImages)
+        $(lastImageBottom).remove()
+        if ($(rightBtn).is(':animated')) {
+            $(rightBtn).prop('disabled', true)
+        };
+        setTimeout(() => {
+            $(rightBtn).prop('disabled', false)
+        }, 500)
+    })
+
+    $(leftBtn).click(() => {
+        $(leftBtn).prop('disabled', true)
+        let firstImageTop = $(".img-top .image").first()
+        $(firstImageTop).clone().appendTo(topImages)
+        $(firstImageTop).remove()
+        let firstImageBottom = $(".img-bot .image").first()
+        $(firstImageBottom).clone().appendTo(bottomImages)
+        $(firstImageBottom).remove()
+        setTimeout(() => {
+            $(leftBtn).prop('disabled', false)
+        }, 500)
+    })
 });
